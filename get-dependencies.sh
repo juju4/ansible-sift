89850ef96685cd28ea022fad8aa6a95a3a09ad2b
juju4 <juju4@users.noreply.github.com>
2017-01-03 15:25:08 -0500

    sync repositories - tentative xenial support but missing sift ppa

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,4 +12,6 @@ fi
 [ ! -d $rolesdir/geerlingguy.java ] && git clone https://github.com/geerlingguy/ansible-role-java.git $rolesdir/geerlingguy.java
 [ ! -d $rolesdir/ernestas-poskus.docker ] && git clone https://github.com/ernestas-poskus/ansible-role-docker.git $rolesdir/ernestas-poskus.docker
 [ ! -d $rolesdir/juju4.gift ] && git clone https://github.com/juju4/ansible-gift.git $rolesdir/juju4.gift
+## galaxy naming
+[ ! -e $rolesdir/juju4.sift ] && ln -s ansible-sift $rolesdir/juju4.sift
 
