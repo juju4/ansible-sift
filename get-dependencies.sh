af3700cd3e2d6e4d315fab85b3328b9a470cbea8
juju4 <juju4@users.noreply.github.com>
2017-01-04 04:41:35 -0500

    fix test in Jenkinsfile+kitchen

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,6 +12,10 @@ fi
 [ ! -d $rolesdir/geerlingguy.java ] && git clone https://github.com/geerlingguy/ansible-role-java.git $rolesdir/geerlingguy.java
 [ ! -d $rolesdir/ernestas-poskus.docker ] && git clone https://github.com/ernestas-poskus/ansible-role-docker.git $rolesdir/ernestas-poskus.docker
 [ ! -d $rolesdir/juju4.gift ] && git clone https://github.com/juju4/ansible-gift.git $rolesdir/juju4.gift
-## galaxy naming
-[ ! -e $rolesdir/juju4.sift ] && ln -s ansible-sift $rolesdir/juju4.sift
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/juju4.sift ] && ln -s ansible-sift $rolesdir/juju4.sift
+[ ! -e $rolesdir/juju4.sift ] && cp -R $rolesdir/ansible-sift $rolesdir/juju4.sift
+
+## don't stop build on this script return code
+true
 
