1b5e5cb3938ee00c96b437b078a18c92aadc420a
juju4 <juju4@users.noreply.github.com>
2017-02-10 16:17:53 -0500

    missing new role dependencies

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,6 +12,8 @@ fi
 [ ! -d $rolesdir/geerlingguy.java ] && git clone https://github.com/geerlingguy/ansible-role-java.git $rolesdir/geerlingguy.java
 [ ! -d $rolesdir/ernestas-poskus.docker ] && git clone https://github.com/ernestas-poskus/ansible-role-docker.git $rolesdir/ernestas-poskus.docker
 [ ! -d $rolesdir/juju4.gift ] && git clone https://github.com/juju4/ansible-gift.git $rolesdir/juju4.gift
+[ ! -d $rolesdir/juju4.volatility ] && git clone https://github.com/juju4/ansible-volatility.git $rolesdir/juju4.volatility
+[ ! -d $rolesdir/juju4.rekall ] && git clone https://github.com/juju4/ansible-rekall.git $rolesdir/juju4.rekall
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.sift ] && ln -s ansible-sift $rolesdir/juju4.sift
 [ ! -e $rolesdir/juju4.sift ] && cp -R $rolesdir/ansible-sift $rolesdir/juju4.sift
