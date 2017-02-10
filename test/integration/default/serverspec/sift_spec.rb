7633bf15549c05cdec9b7a466e3436d1b5a21132
juju4 <juju4@users.noreply.github.com>
2017-02-10 16:08:50 -0500

    review sift-files-install/no remote fileglob, separate pip-upgrade, use external roles for volatility and rekall

--- test/integration/default/serverspec/sift_spec.rb
+++ test/integration/default/serverspec/sift_spec.rb
@@ -20,4 +20,24 @@ end
 describe file('/home/sansforensics/Desktop/sift-cheatsheet.pdf') do
   it { should be_symlink }
 end
+describe file('/home/sansforensics/Desktop/SANS-DFIR.pdf') do
+  it { should be_symlink }
+end
+describe file('/usr/share/tsk/sorter/archives.sort') do
+  it { should be_file }
+end
+describe file('/usr/local/bin/update-sift') do
+  it { should be_file }
+  it { should be_executable }
+end
+describe file('/usr/lib/python2.7/dist-packages/volatility/plugins/autoruns.py') do
+  it { should be_file }
+end
+describe file('/usr/share/sift/images/dfir_logo.png') do
+  it { should be_file }
+end
+describe file('/usr/share/regripper/rip.pl') do
+  it { should be_file }
+  it { should be_executable }
+end
 
